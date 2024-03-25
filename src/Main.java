d5750a25a2a5c5bd23c2774828ac7bd37edc5664
russa89 <natast2189@gmail.com>
2024-03-25 15:11:38 +0300

    Мое ДЗ, уфф

--- src/Main.java
+++ src/Main.java
@@ -52,7 +52,63 @@ int totalWhitePaint = whitePaintPerClass * classesQtty;
 int totalBrownPaint = brownPaintPerClass * classesQtty;
         System.out.println("В школе, где " + classesQtty + " классов, нужно " + totalWhitePaint + " банок белой краски и " +
                 totalBrownPaint + " банок коричневой краски");
+byte bananas = 5;
+byte milk = 2;
+byte egg = 4;
+byte iceCream = 2;
+byte bananaWeight = 80;
+byte eggWeight = 70;
+byte milkWeight = 105;
+byte iceCreamWeight = 100;
+double breakfastWeight = (bananas * bananaWeight) + (milk * milkWeight) + (egg * eggWeight) +
+        (iceCream * iceCreamWeight);
+int oneKg = 1000;
+double breakfastWeightKg = (breakfastWeight / oneKg);
+        System.out.println("Вес такого спортзавтрака равен " + breakfastWeight +
+                " гр. или " + breakfastWeightKg + " кг.");
 
+byte result = 7;
+int resultInGramms = result * oneKg;
+short lossOption1 = 250;
+short lossOption2 = 500;
+int lossResultOption1 = resultInGramms / lossOption1;
+int lossResultOption2 = resultInGramms / lossOption2;
+        System.out.println(lossResultOption1 + " дней уйдет на похудение, " +
+                "если спортсмен будет терять каждый день по 250 грамм");
+        System.out.println(lossResultOption2 + " дней уйдет на похудение, " +
+                "если спортсмен будет терять каждый день по 500 грамм");
+int averageResult = (lossResultOption1 + lossResultOption2) / 2;
+        System.out.println("При применении смешанной схемы у спортсмена на похудение уйдет " +
+                averageResult + " день");
+
+        int salaryMasha = 67760;
+        int salaryDenis = 83690;
+        int salaryKristina = 76230;
+        byte monthsPerYear = 12;
+
+
+        int yearSalaryMasha = salaryMasha * monthsPerYear;
+        int yearSalaryDenis = salaryDenis * monthsPerYear;
+        int yearSalaryKristina = salaryKristina * monthsPerYear;
+
+        int newSalaryMasha = salaryMasha + (salaryMasha * 10 / 100);
+        int yearNewSalaryMasha = newSalaryMasha * monthsPerYear;
+        int yearDifferenceSalaryMasha = yearNewSalaryMasha - yearSalaryMasha;
+        System.out.println("Маша теперь получает " + newSalaryMasha + " рублей. Годовой доход вырос" +
+                " на " + yearDifferenceSalaryMasha + " рублей.");
+
+        int newSalaryDenis = salaryDenis + (salaryDenis * 10 / 100);
+        int yearNewSalaryDenis = newSalaryDenis * monthsPerYear;
+        int yearDifferenceSalaryDenis = yearNewSalaryDenis - yearSalaryDenis;
+        System.out.println("Денис теперь получает " + newSalaryDenis + " рублей. Годовой доход вырос" +
+                " на " + yearDifferenceSalaryDenis + " рублей.");
+
+        int newSalaryKristina = salaryKristina + (salaryKristina * 10 / 100);
+        int yearNewSalaryKristina = newSalaryKristina * monthsPerYear;
+        int yearDifferenceSalaryKristina = yearNewSalaryKristina - yearSalaryKristina;
+        System.out.println("Кристина теперь получает " + newSalaryKristina + " рублей. Годовой доход вырос" +
+                " на " + yearDifferenceSalaryKristina + " рублей.");
+         ;
 
 
     }
